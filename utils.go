9b0f13141a04554528433eeea91d10c49c09f4a5
harsh <hg@gatech.edu>
2018-06-16 15:13:55 -0400

    moved encryption to seperate file

--- utils.go
+++ utils.go
@@ -1,12 +1,6 @@
 package main 
 
 import "bytes"
-import "crypto"
-import "crypto/aes"
-import "crypto/cipher"
-import "crypto/rand"
-import "crypto/rsa"
-import "crypto/sha256"
 import "errors"
 import "fmt"
 import "github.com/pierrre/archivefile/zip"
@@ -60,52 +54,6 @@ func bucket_exists(dirname string)(bool){
 	return true
 }
 
-func generate_key_pair()(*rsa.PrivateKey,*rsa.PublicKey,error){
-	privkey, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err!=nil{
-		return nil,nil,err
-	}
-	pubkey := &privkey.PublicKey
-	return privkey,pubkey,err
-}
-
-func encrypt_with_pubkey(msg []byte,priv_key *rsa.PrivateKey,rec_key *rsa.PublicKey)(*[]byte,*[]byte,error){
-    label := []byte("")
-	hash := sha256.New()
-	ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, rec_key, msg, label)
-	if err!=nil{
-		return nil,nil,err
-	}
-	var opts rsa.PSSOptions
-	opts.SaltLength = rsa.PSSSaltLengthAuto
-	PSSmessage := msg
-	newhash := crypto.SHA256
-	pssh := newhash.New()
-	pssh.Write(PSSmessage)
-	hashed := pssh.Sum(nil)
-	signature, sign_err := rsa.SignPSS(rand.Reader, priv_key, newhash, hashed, &opts)
-	if sign_err!=nil{
-		return nil,nil,sign_err
-	} 
-	return &ciphertext,&signature,nil
-}
-
-func decrypt_with_privkey(priv_key *rsa.PrivateKey,ciphertext *[]byte)(*[]byte,error){
-	//Decrypt Ciphertext
-	hash := sha256.New()
-	label := []byte("")
-	plainText, err := rsa.DecryptOAEP(hash, rand.Reader, priv_key, *ciphertext, label)
-	if err!=nil{
-		return nil,err
-	}
-	// Verify Signature
-	// TODO
-	// newhash := crypto.SHA256
-	// err = rsa.VerifyPSS(sender_pubkey, newhash, hashed, signature, &opts)
-
-	return &plainText,nil
-}
-
 
 func get_size(path string) (int64, error) {
 	/*Gets the size of directory*/
@@ -150,57 +98,7 @@ func dir_to_compressed_bytes(dirname string)(*[]byte,error){
 
 }
 
-func Encrypt(plaintextptr *[]byte, key *[32]byte) (*[]byte, error) {
-	/* Encrypts a byte array with a key*/
 
-	var plaintext []byte = *plaintextptr
-
-	block, err := aes.NewCipher(key[:])
-	if err != nil {
-		return nil, err
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {
-		return nil, err
-	}
-
-	ciphertext:= gcm.Seal(nonce, nonce, plaintext, nil)
-	return &ciphertext,nil
-}
-
-func Decrypt(ciphertextptr *[]byte, key *[32]byte) (*[]byte,error) {
-	/* Decrypts a byte array with a key*/
-
-	var ciphertext []byte = *ciphertextptr
-
-	block, err := aes.NewCipher(key[:])
-	if err != nil {
-		return nil, err
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(ciphertext) < gcm.NonceSize() {
-		return nil, errors.New("malformed ciphertext")
-	}
-
-	plaintext,plaintexterr:= gcm.Open(nil,
-		ciphertext[:gcm.NonceSize()],
-		ciphertext[gcm.NonceSize():],
-		nil,
-	)
-	return &plaintext,plaintexterr
-} 
 
 
 
